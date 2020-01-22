22269e9e40d205e64e44855c6ad95148878c2914
Aditprayogo <adit.ihzar@gmail.com>
2020-01-22 14:59:38 +0700

    adding onboarding two screen

---
+++ app/src/main/java/com/aditprayogo/movapps/onboarding/OnboardingTwoActivity.kt
@@ -0,0 +1,13 @@
+package com.aditprayogo.movapps.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aditprayogo.movapps.R
+
+class OnboardingTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
