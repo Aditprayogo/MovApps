f3e19d48b179aa2f5b27215a76dabbc35af07f9f
Aditprayogo <adit.ihzar@gmail.com>
2020-01-25 02:03:51 +0700

    adding seat layouts

---
+++ app/src/main/java/com/aditprayogo/movapps/SeatScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.aditprayogo.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeatScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_screen)
+    }
+}
